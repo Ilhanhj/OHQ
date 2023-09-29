7f0143ee0b1ace67f47c544a1cd714a6d02f7fa4
ilhanhj <ilhanzuldan11@gmail.com>
2023-09-29 14:25:10 +0700

    upload web

---
+++ script.js
@@ -0,0 +1,689 @@
+const quizData = [
+  {
+    // reverse
+    question: "Saya tidak merasa senang dengan keadaan saya saat ini.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Saya sangat tertarik pada orang lain.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya merasa hidup ini sangat bermanfaat.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya mempunyai perasaan yang sangat hangat terhadap hampir semua orang.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+    question: "Saya jarang bangun dengan perasaan istirahat.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    // reverse
+    question: "Saya tidak terlalu optimis tentang masa depan.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Menurut saya banyak hal yang lucu dan menghibur.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya selalu berkomitmen dan terlibat.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Hidup itu baik.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+
+    question: "Menurutku dunia itu bukan tempat yang baik.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Saya banyak tertawa.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya sangat puas dengan segala hal dalam hidup saya.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+
+    question: "Menurutku, penampilanku tidak menarik.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    // reverse
+
+    question: "Ada kesenjangan antara apa yang ingin saya lakukan dan apa yang telah saya lakukan.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Saya sangat senang.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya menemukan keindahan dalam beberapa hal.",
+    options: {
+      a: "Sangat tidak setuju",
+      poinA: 1,
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya selalu memberikan efek ceria pada orang lain.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya bisa cocok dengan semua yang saya mau.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+
+    question: "Saya merasa bahwa saya tidak terlalu bisa mengendalikan hidup saya.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Saya merasa mampu melakukan apapun.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya merasa sepenuhnya waspada secara mental.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya sering mengalami kegembiraan dan girang hati.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+
+    question: "Saya tidak merasa mudah dalam dalam mengambil keputusan.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    // reverse
+
+    question: "Saya tidak mempunyai arti dan tujuan tertentu dalam hidup.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    question: "Saya merasa memiliki banyak energi.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    question: "Saya biasanya memiliki pengaruh yang baik pada berbagai acara.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 1,
+    poinB: 2,
+    poinC: 3,
+    poinD: 4,
+    poinE: 5,
+    poinF: 6,
+  },
+  {
+    // reverse
+
+    question: "Saya tidak bersenang-senang dengan orang lain.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    // reverse
+
+    question: "Saya merasa tidak sehat.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+  {
+    // reverse
+
+    question: "Saya tidak memiliki kenangan indah tentang masa lalu.",
+    options: {
+      a: "Sangat tidak setuju",
+      b: "Agak tidak setuju",
+      c: "Sedikit tidak setuju",
+      d: "Sedikit setuju",
+      e: "Cukup setuju",
+      f: "Sangat setuju",
+    },
+    poinA: 6,
+    poinB: 5,
+    poinC: 4,
+    poinD: 3,
+    poinE: 2,
+    poinF: 1,
+  },
+];
+
+const questionEl = document.querySelector("#question");
+const a_text = document.querySelector("#a_text");
+const b_text = document.querySelector("#b_text");
+const c_text = document.querySelector("#c_text");
+const d_text = document.querySelector("#d_text");
+const e_text = document.querySelector("#e_text");
+const f_text = document.querySelector("#f_text");
+const btnNext = document.querySelector("#next");
+const btnRepeat = document.querySelector("#repeat");
+const answerEls = document.querySelectorAll("#answer");
+const quiz = document.querySelector("#quiz-card");
+const choices = document.querySelector("#choices");
+const lis = document.querySelectorAll("li");
+
+let currentQuiz = 0;
+let score = 0;
+loadQuiz();
+function loadQuiz() {
+  unSelected();
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+
+  a_text.innerText = quizData[currentQuiz].options.a;
+  b_text.innerText = quizData[currentQuiz].options.b;
+  c_text.innerText = quizData[currentQuiz].options.c;
+  d_text.innerText = quizData[currentQuiz].options.d;
+  e_text.innerText = quizData[currentQuiz].options.e;
+  f_text.innerText = quizData[currentQuiz].options.f;
+}
+
+function getSelected() {
+  const answerEls = document.querySelectorAll("#answer");
+  let answer = undefined;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function unSelected() {
+  const answerEls = document.querySelectorAll("#answer");
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answerEl.checked = false;
+    }
+  });
+}
+
+function scoring() {
+  const answerEls = document.querySelectorAll("#answer");
+  if (answerEls[0].checked) {
+    score += quizData[currentQuiz].poinA;
+  } else if (answerEls[1].checked) {
+    score += quizData[currentQuiz].poinB;
+  } else if (answerEls[2].checked) {
+    score += quizData[currentQuiz].poinC;
+  } else if (answerEls[3].checked) {
+    score += quizData[currentQuiz].poinD;
+  } else if (answerEls[4].checked) {
+    score += quizData[currentQuiz].poinE;
+  } else if (answerEls[5].checked) {
+    score += quizData[currentQuiz].poinF;
+  } else {
+    return;
+  }
+  total = score / 29;
+}
+
+btnNext.addEventListener("click", () => {
+  const answer = getSelected();
+  scoring();
+
+  if (answer) {
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    }
+    if (!(currentQuiz < quizData.length) && total >= 1) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/sad.png" alt="" srcset="" style="width: 80px" />
+    </h1>
+    <h1 class="result" id="result">Tidak Bahagia</h1>
+    <p class="result-text" id="result-text">
+    Tidak bahagia. Kamu mungkin melihat diri Kamu dan situasi Kamu lebih buruk daripada yang sebenarnya.
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+    </div>`;
+    } else if (!(currentQuiz < quizData.length) && total >= 2) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/bitsad.png" alt="" srcset="" style="width: 80px" /></h1>
+    <h1 class="result" id="result">Agak Tidak Bahagia</h1>
+    <p class="result-text" id="result-text">
+    Cobalah membuat jurnal rasa syukur atau daftar rasa syukur, dan mulailah mensyukuri apapun yang kamu miliki.
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+    </div>
+`;
+    } else if (!(currentQuiz < quizData.length) && total >= 3) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/netral.png" alt="" srcset="" style="width: 80px" /></h1>
+    <h1 class="result" id="result">Netral</h1>
+    <p class="result-text" id="result-text">
+    Kamu Tidak benar-benar bahagia, cobalah untuk lebih sering tersenyum dan perbanyak tawa. 
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+    </div>
+    `;
+    } else if (!(currentQuiz < quizData.length) && total >= 4) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/bithappy.png" alt="" srcset="" style="width: 80px" /></h1>
+    <h1 class="result" id="result">Cukup Bahagia</h1>
+    <p class="result-text" id="result-text">
+    Kamu cukup bahagia - puas. Ini adalah skor rata-rata orang.
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+      </div>`;
+    } else if (!(currentQuiz < quizData.length) && total >= 5) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/happy.png" alt="" srcset="" style="width: 80px" /></h1>
+    <h1 class="result" id="result">Sangat Bahagia</h1>
+    <p class="result-text" id="result-text">
+    Kamu Sangat bahagia. Bahagia memiliki manfaat lebih dari sekadar merasa baik. Hal ini berkorelasi dengan peningkatan kesehatan, pernikahan yang lebih baik, dan pencapaian tujuan Kamu. Kebahagiaan dasar memungkinkan Kamu untuk berkembang dan membangun menuju kesuksesan yang lebih besar.
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+    </div>`;
+    } else if (!(currentQuiz < quizData.length) && total >= 6) {
+      choices.innerHTML = `
+      <div class="quiz-card" id="quiz-card">
+    <h2 class="result-title" id="result-title">Hasil</h2>
+    <h1 class="emot" id="emot"><img src="images/toohappy.png" alt="" srcset="" style="width: 80px" /></h1>
+    <h1 class="result" id="result">Terlalu Bahagia</h1>
+    <p class="result-text" id="result-text">
+    Kamu Terlalu bahagia. Itu benar - mungkin saja Anda terlalu bahagia. Penelitian tampaknya menunjukkan bahwa ada tingkat kebahagiaan yang ideal untuk bekerja dengan baik di tempat kerja atau sekolah, atau untuk kesehatan Anda, dan bahwa terlalu bahagia dapat dikaitkan dengan kinerja yang lebih buruk di bidang-bidang ini.
+    </p>
+    <div class="button">
+    <button onclick="location.reload()" class="btn-repeat" >Ulangi <i class="fa-solid fa-repeat" style="color: #fff"></i></button>
+    </div>
+    </div>`;
+    }
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops",
+      text: "Pertanyaan nya di isi dulu yaa !",
+      confirmButtonText: "OK",
+    });
+  }
+});
